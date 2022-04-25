e6303009834f85d80e059df87f25f43efe814fb4
rova <nasrallahrahaf7@gmail.com>
2022-04-25 17:06:12 +0300

    Initial commit

---
+++ src/main/java/main/project/HelloApplication.java
@@ -0,0 +1,25 @@
+package main.project;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 986, 731);
+        stage.setTitle("Al'Awda Sports Club");
+        stage.getIcons().add(new Image("icon.png"));
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module main.project {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens main.project to javafx.fxml;
+    exports main.project;
+}
